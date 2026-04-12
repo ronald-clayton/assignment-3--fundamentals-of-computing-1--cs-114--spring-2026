27dd597a4fe918040a40c92a019d2857ed0de171
ronald-clayton <ronaldclayton3rd@gmail.com>
2026-04-12 19:20:52 -0400

    Matrix update

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,39 @@
 public class Matrix {
+  private int [] [] matrix;
+
+  public Matrix(int size) {
+    matrix = new int [size] [size];
+    System.out.println("Your matrix is " + size + "x" + size + "\n");
+  }
+
+  public void populateMatrix() {
+    int value = 1;
+    int size = matrix.length;
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        matrix[row] [col] = value++;
+      }
+    }
+  }
+
+  private void swap(int x1, int y1, int x2, int y2) {
+    int temp = matrix [x1] [y1];
+    matrix [x1] [y1] = matrix [x2] [y2];
+    matrix [x2] [y1] = temp;
+  }
+
+  public void flipMatrix(); {
+    int size = matrix.length;
+
+    for (int row = 0; row, size; row++) {
+      for (int col = 0; col < size; col++) {
+        if ( row + col < size - 1) {
+          int swapR = size - 1 - col;
+          int swapC = size - 1 - row;
+          swap(row, col, swapR, swapC);
+        }
+      }
+    }
+  }
+
 }
