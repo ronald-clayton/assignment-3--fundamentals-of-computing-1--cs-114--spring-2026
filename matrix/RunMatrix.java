597962635e5a3f6f85ad578be83394a65bf74073
ronald-clayton <ronaldclayton3rd@gmail.com>
2026-04-12 18:51:04 -0400

    Updates to run matrix code

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -2,5 +2,26 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("\nPlease enter the size of your matrix:");
+    int size = input.nextInt();
+
+    Matrix mymatrix = new Matrix(size);
+
+    System.out.println("\nPrinting matrix with default values:");
+    mymatrix.printMatrix();
+
+    System.out.println("\nPopulating matrix...matrix populated")
+    System.out.println("\nPrinting matrix:");
+    mymatrix.populateMatrix();
+    mymatrix.printMatrix();
+
+    System.out.println("\nFlipping matrix...matrix flipped");
+    System.out.println("\nPrinting Flipped matrix:");
+    mymatrix.flipmatrix();
+    mymatrix.printMatrix();
+
+    input.close();
   }
 }
